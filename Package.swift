716f5e41684594abbddec386589c725e73d2bcb3
Robin Goos <robingoos@gmail.com>
2020-12-02 19:30:47 +0100

    Remove package.swift comments

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "LaserDisc",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LaserDisc",
             targets: ["LaserDisc"]),
@@ -16,8 +15,6 @@ let package = Package(
         .package(url: "https://github.com/envoy/Ambassador.git", from: "4.0.5")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LaserDisc",
             dependencies: ["Embassy", "Ambassador"]),
